e234eb5c6f131806664caa52142a3a6c440dbbbf
plp-kolyan <plp-kolyan@mail.ru>
2024-11-07 16:57:00 +0700

    Отправка сообщения в телекграмм без обработки событий

--- simplebot/admin.py
+++ simplebot/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from simplebot.models import Answer
+from simplebot.models import Answer, SendMessage
 
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
     pass
 # Register your models here.
+
+@admin.register(SendMessage)
+class SendMessageAdmin(admin.ModelAdmin):
+    pass
--- simplebot/models.py
+++ simplebot/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+import requests
+from serveses_config import bot_token
+
 
 # Create your models here.
 class Answer(models.Model):
     command = models.CharField(max_length=10)
     text = models.TextField()
+
+class SendMessage(models.Model):
+    URL = ("https://api.telegram.org/bot"
+           + bot_token
+           + "/sendMessage")
+    chat_id = models.CharField(max_length=25)
+    text = models.TextField()
+
+    def save(self, *args, **kwargs):
+        requests.post(self.URL, data={
+            "chat_id": self.chat_id,
+            "text": self.text
+        })
+        return super().save(*args,
+                            **kwargs)
